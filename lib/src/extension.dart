3e821e4e19ded11b25d06578126f253654f2914f
Juan Antonio Ramirez <tonior@gmail.com>
2020-01-02 18:04:46 -0400

    Fix for dartdoc

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -1,5 +1,6 @@
 part of optional_internal;
 
-extension OptionalExtension<T> on T {
+extension OptionalExtension<T extends Object> on T {
+  /// Returns an empty Optional if called on `null`, otherwise returns an Optional with `this` as its value
   Optional<T> get toOptional => Optional.ofNullable(this);
 }
